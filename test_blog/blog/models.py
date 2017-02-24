30e43adcd6b887af5b6f7341ed876f17e15b74ff
rewenset <a.zakurenyi@gmail.com>
2017-02-24 14:24:39 +0200

    init commit

---
+++ test_blog/blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
